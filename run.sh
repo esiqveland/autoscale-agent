9d91b3422cc7ee594c220e8fd26738ea3063a126
Andreas Baakind <toraba@ifi.uio.no>
2012-11-09 12:50:45 +0100

    Renamed log4j-properties file

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="no.uio.master.autoscale.slave.AutoscaleSlave" -Dlog4j.configuration=log4j-autoscale.properties -Djava.library.path=/Users/andreas/UiO/gitrepos/autoscale-slave/lib/
+mvn exec:java -Dexec.mainClass="no.uio.master.autoscale.slave.AutoscaleSlave" -Dlog4j.configuration=log4j.properties -Djava.library.path=/Users/andreas/UiO/gitrepos/autoscale-slave/lib/
